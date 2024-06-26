d1f9105b58f99512bd755e61d1bed888a1c375ad
SteevenRaj23 <steevenraj.sr23@gmail.com>
2024-06-26 11:29:29 +0530

    vercel2

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => {
+    res.send('Hello from Express!');
+  });
+  
 
 app.post("/login", async (req, resp) => {
     console.log(req.body);
