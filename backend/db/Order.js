fae4cee1226c32edef786ffd0f5242973629503f
SteevenRaj23 <steevenraj.sr23@gmail.com>
2024-08-02 17:40:20 +0530

    css -6

--- backend/db/Order.js
+++ backend/db/Order.js
@@ -1,27 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ProductSchema = new mongoose.Schema({
-  // Define your product schema here
+  productId: String,
+  qty: Number,
 });
 
 const OrderSchema = new mongoose.Schema({
   userId: String,
   userOrderNumber: { type: String, unique: true },
   products: [ProductSchema],
-  status: { type: String, default: 'pending' },
-  createdAt: { type: String, default: '' },
+  status: { type: String, default: "pending" },
+  createdAt: { type: String, default: "" },
 });
 
 // Middleware to format createdAt before saving
-OrderSchema.pre('save', function (next) {
+OrderSchema.pre("save", function (next) {
   const date = new Date();
-  const options = { hour12: true, hour: 'numeric', minute: 'numeric', second: 'numeric' };
-  const timeString = date.toLocaleTimeString('en-US', options);
-  const dateString = date.toLocaleDateString('en-US');
+  const options = {
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+  };
+  const timeString = date.toLocaleTimeString("en-US", options);
+  const dateString = date.toLocaleDateString("en-US");
   this.createdAt = `${dateString}, ${timeString}`;
   next();
 });
 
-const Order = mongoose.model('Order', OrderSchema);
+const Order = mongoose.model("Order", OrderSchema);
 
 module.exports = Order;
